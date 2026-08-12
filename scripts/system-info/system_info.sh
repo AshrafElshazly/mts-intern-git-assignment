3d18c574542cae9f2b09f6708bfeaed09c308a43
Adham <Adhamfawaz580@gmail.com>
2026-08-12 12:55:04 +0000

    Add system information script

---
+++ scripts/system-info/system_info.sh
@@ -0,0 +1,29 @@
+##!/bin/bash
+
+
+
+echo "===== System Information ====="
+
+
+
+echo "Hostname:"
+
+hostname
+
+
+
+echo "Current User:"
+
+whoami
+
+
+
+echo "Kernel Version:"
+
+uname -r
+
+
+
+echo "Uptime:"
+
+uptime
